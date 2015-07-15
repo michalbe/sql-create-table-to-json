8bdeaff8f736ce607378ece3fcc6dfb5473ed461
Michał Budzyński <michal@virtualdesign.pl>
2015-07-15 01:01:00 +0100

    :goat: callback support added

--- index.js
+++ index.js
@@ -1,27 +1,29 @@
 'use strict';
-var output = {};
 
-var fs = require('fs');
-fs.readFile('data.sql', 'utf8', function (err,data) {
-  if (err) {
-    return console.log(err);
-  }
-  var chunks = data.split('\n\n');
-  chunks.forEach(function(chunk){
-    var tableName = chunk.match('CREATE TABLE `(.*)`')[1];
-    output[tableName] = [];
-    var lines = chunk.split('\n');
-    lines.forEach(function(line, index){
-      if (~line.indexOf(' KEY ') || index === 0) {
-        return;
-      }
+var sqt2j = function(file, cb){
+  var output = {};
 
-      var match = line.match(' `(.*)`');
-      if (match) {
-        output[tableName].push(match[1]);
-      }
+  var fs = require('fs');
+  fs.readFile('data.sql', 'utf8', function (err,data) {
+    if (err) {
+      return cb(err);
+    }
+    
+    var chunks = data.split('\n\n');
+    chunks.forEach(function(chunk){
+      var tableName = chunk.match('CREATE TABLE `(.*)`')[1];
+      output[tableName] = [];
+      var lines = chunk.split('\n');
+      lines.forEach(function(line, index){
+        if (~line.indexOf(' KEY ') || index === 0) {
+          return;
+        }
+
+        var match = line.match(' `(.*)`');
+        if (match) {
+          output[tableName].push(match[1]);
+        }
+      });
     });
   });
-
-  // console.log(data);
-});
+};
