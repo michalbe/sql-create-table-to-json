fd2efd1b26a9769c65d6761739e6aef755cf30f0
Michał Budzyński <michal@virtualdesign.pl>
2015-07-15 01:01:00 +0100

    :ticket: more complex data

--- data.sql
+++ data.sql
@@ -7,3 +7,13 @@ CREATE TABLE `dev_access` (
  `delete` int(11) NOT NULL,
  PRIMARY KEY (`id_profile`,`id_tab`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8
+
+CREATE TABLE `dev_access` (
+ `id_profile` int(10) unsigned NOT NULL,
+ `id_tab` int(10) unsigned NOT NULL,
+ `view` int(11) NOT NULL,
+ `add` int(11) NOT NULL,
+ `edit` int(11) NOT NULL,
+ `delete` int(11) NOT NULL,
+ PRIMARY KEY (`id_profile`,`id_tab`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
