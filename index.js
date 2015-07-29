c76bba965c1794d3cba763df408f233222d82f80
Michał Budzyński <michal@virtualdesign.pl>
2015-07-29 01:01:00 +0100

    :fire: empty line issue fix

--- index.js
+++ index.js
@@ -18,19 +18,22 @@ var sct2j = function(file, cb){
     //return;
     var chunks = data.split('\n\n');
     chunks.forEach(function(chunk){
-      var tableName = chunk.match('CREATE TABLE `(.*)`')[1];
-      output[tableName] = [];
-      var lines = chunk.split('\n');
-      lines.forEach(function(line, index){
-        if (~line.indexOf(' KEY ') || index === 0) {
-          return;
-        }
+      var tableName = chunk.match('CREATE TABLE `(.*)`');
+      if (tableName) {
+        tableName = tableName[1];
+        output[tableName] = [];
+        var lines = chunk.split('\n');
+        lines.forEach(function(line, index){
+          if (~line.indexOf(' KEY ') || index === 0) {
+            return;
+          }
 
-        var match = line.match(' `(.*)`');
-        if (match) {
-          output[tableName].push(match[1]);
-        }
-      });
+          var match = line.match(' `(.*)`');
+          if (match) {
+            output[tableName].push(match[1]);
+          }
+        });
+      }
     });
 
     cb(null, output);
